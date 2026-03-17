c9b27f5808a5dc6606ccaa69e899bd923c859572
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-03-17 15:37:45 -0300

    fix: remove source attribute from ReadingPassage model

---
+++ prisma/migrations/20260317183713_remove_passage_source/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `source` on the `ReadingPassage` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ReadingPassage" DROP COLUMN "source";
